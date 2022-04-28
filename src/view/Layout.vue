83abb02e89efc871832bf996312ef3f82e9a11ea
yooogooo <1099360571@qq.com>
2022-04-29 00:16:34 +0800

    基本完善Layout

--- src/view/Layout.vue
+++ src/view/Layout.vue
@@ -1,14 +1,18 @@
 <template>
-  <el-container style="height: 614px; border: 1px solid #eee">
+  <el-container style="height: 650px; border: 1px solid #eee">
       <el-header class="header" height="64px">
-        <el-col :span="4">1</el-col>
-        <el-col :span="4" style="height:100%; background-color:blue">
-          <el-col :span="12">Stos Aristov</el-col>
-          <el-col :span="4"><el-avatar  shape="square" :size="size" :src="squareUrl"></el-avatar></el-col>
+        <el-col :span="6">
+          <div class="image-logo">
+            <el-image :src="'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'"></el-image>
+          </div>
+        </el-col>
+        <el-col :span="4" style="height:100%; background-color:#fff;">
+          <el-col :span="14"><span class="user-name">Stos Aristov</span></el-col>
+          <el-col :span="4"><el-avatar  shape="square" :size="size" :src="squareUrl" class="user-avatar"></el-avatar></el-col>
           <el-col :span="2">
-            <el-dropdown>
+            <el-dropdown class="user-more">
                 <span class="el-dropdown-link">
-                <i class="el-icon-arrow-down el-icon--right"></i>
+                <i class="el-icon-arrow-down"></i>
               </span>
               <el-dropdown-menu>
                 <el-dropdown-item command="a">黄金糕</el-dropdown-item>
@@ -18,31 +22,33 @@
           </el-dropdown>
           </el-col>
         </el-col>
-        <el-col :span="10">4</el-col>
         <el-col :span="3">
-          <el-input
+            <el-input
               type="text"
               prefix-icon="el-icon-search"
               v-model="kb_content"
               placeholder="请输入"
               @keyup.enter.native="getAllKb"
+              class="search-bar"
             ></el-input>
         </el-col>
-        <el-col :span="1">
-          <i class="el-icon-message-solid"></i>
+        <el-col :span="1" style="float:right;">
+          <div class="message-icon"><i class="el-icon-message-solid"></i></div>
         </el-col>
       </el-header>
     <el-container>
-      <el-aside class="tabbar" width="160px">
+      <el-aside class="tabbar" width="210px">
         <el-row>
-          <el-menu
+          <div class="navbar">
+            <el-menu
             default-active="2"
-            class="el-menu-vertical-demo"
+            class="el-menu-vertical-demo navbar-menu"
             @open="handleOpen"
             @close="handleClose"
-            background-color="#545c64"
-            text-color="#fff"
-            active-text-color="#ffd04b">
+            background-color="#fff"
+            text-color="#000"
+            active-text-color="#4000e3"
+            >
             <el-menu-item index="1"  @click="navToHome">
               <i class="el-icon-menu"></i>
               <span slot="title">首页</span>
@@ -64,13 +70,24 @@
               <span slot="title">地图</span>
             </el-menu-item>
           </el-menu>
+          </div>
         </el-row>
         <el-row>
-          <el-col span='12'><span>Create Activity</span></el-col>
-          <el-col span='12'><span>image</span></el-col>
+          <div class="createA">
+            <el-col span='12'><span class="text-CA">Create Activity</span></el-col>
+            <el-col span='12'>
+              <div class="image-CA">
+              <el-image :src="'/static/plus.png'" style="position:relative; bottom:4px; left:1px;"></el-image>
+            </div>
+            </el-col>
+          </div>
         </el-row>
         <el-row>
-          <el-col :span="24"><span>image</span></el-col>
+          <el-col span='12'>
+            <div class="image-footer">
+              <el-image :src="'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'"></el-image>
+            </div>
+          </el-col>
         </el-row>
       </el-aside>
       <el-main class="main">
@@ -102,15 +119,106 @@ export default Layout
     padding: 0;
   }
 
+  .image-logo{
+    width: 80px;
+    position: relative;
+    top: 5px;
+    left: 25px;
+  }
+
+  .user-name{
+    position: relative;
+    top: 22px;
+    left: 30px;
+    font-weight: 700;
+    font-size: 15px;
+  }
+
+  .user-avatar{
+    position: relative;
+    top:15px;
+  }
+
+  .user-more{
+    position: relative;
+    top: 22px;
+    left: 13px;
+  }
+
+  .message-icon{
+    float: right;
+    position: relative;
+    top: 22px;
+    right: 120px;
+  }
+
+  .search-bar{
+    position: relative;
+    top: 12px;
+    left: 450px;
+  }
+
+  .navbar{
+    font-weight: 700;
+    width: 180px;
+    position: relative;
+    top: 50px;
+    left: 15px;
+    border: 2px solid #333333;
+    border-radius: 16px;
+  }
+
+  .navbar-menu{
+    margin: 10px;
+  }
+
+  .createA{
+    font-weight: 700;
+    width: 180px;
+    height: 70px;
+    border: 2px solid #333333;
+    border-radius: 16px;
+    position: relative;
+    top: 80px;
+    left: 15px;
+  }
+
+  .text-CA{
+    position: relative;
+    top: 10px;
+    left: 15px;
+  }
+
+  .image-CA{
+    width: 50px;
+    height: 40px;
+    background-color: #4000ec;
+    border-radius: 10px;
+    position: relative;
+    top: 13px;
+    left: 20px;
+  }
+
+  .image-footer{
+    width: 150px;
+    position: relative;
+    top: 100px;
+    left: 25px;
+  }
+
+  .el-menu-item .span:hover{
+    background-color: #fff;
+  }
+
   .header{
-    background-color: red;
+    background-color: #fff;
   }
 
   .tabbar{
-    background-color: blue;
+    background-color: #fff;
   }
 
   .main{
-    background-color: yellow;
+    background-color: #f6f6f7;
   }
 </style>
